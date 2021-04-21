c6418cae345426d6ebd14018dcba9337cf777dc0
katringht <katrin_tko@mail.ru>
2021-04-21 21:30:34 +0300

    add orientation

--- app_practice/lib/main.dart
+++ app_practice/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:app_practice/screens/loginScreen/loginScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
